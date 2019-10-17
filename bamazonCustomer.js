ec83b9dd1a028a63584fcc9679ed421a7043f658
Joel Buentello <joel.buentello@yahoo.com>
2019-10-17 14:46:58 -0500

    Added questions for users using inquire.prompt, also console logged to make sure the questions came up

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,6 +3,7 @@ var inquirer = require ('inquirer');
 // var table = require ('console.table');
 var db = mysql.createConnection({
   host     : '127.0.0.1',
+  port     : 3306,
   user     : 'root',
   password : 'Saleen_454',
   database : 'bamazon'
@@ -10,34 +11,41 @@ var db = mysql.createConnection({
 
 var itemArray = [];
  
-db.connect();
+// db.connect();
 
 //Pull the three items selected from database
-db.query('SELECT item_id, product_name, price FROM products', function (error, results, fields) {
-  if (error) throw error;
-  console.log(results);
-})
+db.query('SELECT item_id, product_name, price FROM products', function (error, response, fields) {
+  // console.log(results);
+
   if (!error){
     for (var i = 0; i < response.length; i++){
       itemArray.push({
-          id: response[i].item_id,
-          item: response[i].product_name,
-          price: response[i].price,
-          department: response[i].department_name
+        id: response[i].item_id,
+        item: response[i].product_name,
+        department: response[i].department_name,
+        price: response[i].price,
+        quantity: response[i].price
       });
+    }
   }
-    console.table(itemArray);
-    question1();
-
-  //Set up questions for users
-
-// db.connect(function(error) {
-//   if (error) throw error;
-//   db.query('SELECT * FROM products', 
-// function (error, results, fields) {
-//     if (error) console.log('OH SHIT');
-//     console.log(results);
-//   });
-// });
- 
+  else { throw error; }
+  console.table(itemArray);
+});    
+  
+function questionsForUsers() {
+  inquirer.prompt([
+      {
+          type: 'input',
+          name: 'itemSelected',
+          message: 'Enter the item ID of the product you would like to purchase.'
+      },
+      {
+          type: 'input',
+          name: 'quantity',
+          message: 'How many of this item would you like?'
+      }
+    ]);
+  };
+    // ]).then
+ questionsForUsers()
 db.end();
