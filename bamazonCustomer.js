a85c21e1b47d75f661186591bdb3ff80f5901707
Joel Buentello <joel.buentello@yahoo.com>
2019-10-17 16:07:18 -0500

    Added data to MySQl, fixed errors coming from else statements, and called functions. Now not able to see prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,10 +11,10 @@ var db = mysql.createConnection({
 
 var itemArray = [];
  
-// db.connect();
+db.connect();
 
 //Pull the three items selected from database
-db.query('SELECT item_id, product_name, price FROM products', function (error, response, fields) {
+db.query('SELECT item_id,department_name,stock_quantity, product_name, price FROM products', function (error, response, fields) {
   // console.log(results);
 
   if (!error){
@@ -24,7 +24,7 @@ db.query('SELECT item_id, product_name, price FROM products', function (error, r
         item: response[i].product_name,
         department: response[i].department_name,
         price: response[i].price,
-        quantity: response[i].price
+        quantity: response[i].stock_quantity
       });
     }
   }
@@ -33,19 +33,52 @@ db.query('SELECT item_id, product_name, price FROM products', function (error, r
 });    
   
 function questionsForUsers() {
-  inquirer.prompt([
-      {
-          type: 'input',
-          name: 'itemSelected',
-          message: 'Enter the item ID of the product you would like to purchase.'
-      },
-      {
-          type: 'input',
-          name: 'quantity',
-          message: 'How many of this item would you like?'
+  inquirer.prompt([{
+    type: 'input',
+    name: 'itemSelected',
+    message: 'Enter the item ID of the product you would like to purchase.'
+  },
+  {
+    type: 'input',
+    name: 'quantity',
+    message: 'How many of this item would you like?'
+  }
+
+  ]).then(function(userResponse){
+    var item = parsInt(userResponse.itemSelected);
+    var quantity = userResponse.quantity;
+    db.query('SELECT * FROM products WHERE item-id = ${item}', function (error, response){
+      if (error){
+        console.log("Error");
+        return;
       }
-    ]);
-  };
-    // ]).then
- questionsForUsers()
+      else {
+        itemData = response[0];
+        console.log('This product is in stock, we have ${itemData.product_name} in stock.')
+        var itemsLeft = itemData.stock_quantity - quantity
+        var price = quantity*itemData.price
+        db.query('UPDATE products SET stock_quantity = ${itemsLeft} WHERE item_id = ${item}', function(error, repsonse){
+          if (error){
+            console.log("Update Failed check quantity");
+            return; 
+          }
+          else{
+            console.log(`Your total cost is $${price}.`)
+            var newSales = productData.product_sales + price
+            db.query(`UPDATE products SET product_sales = ${newSales} WHERE item_id = ${item}`, function(error,response){
+              if (error){
+                console.log("Quantity update failed.");
+                return;
+              }
+              else{
+                console.log('Sorry for the inconvenience, but we only have ${itemData.stock_quantity} available')
+                questionsForUsers()
+              }
+            }); 
+          }
+        });
+      }
+    });
+  });
+}
 db.end();
